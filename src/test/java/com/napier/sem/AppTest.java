17278335074273401708599f9e7c5e4e45bee6ef
Dillon-Brown <dillon.brown@salesagility.com>
2019-01-29 10:12:51 +0000

    Test scaffolding

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,5 +1,6 @@
 package com.napier.sem;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -8,13 +9,171 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest
-{
+public class AppTest {
     static App app;
 
     @BeforeAll
-    static void init()
-    {
+    static void init() {
         app = new App();
+    }
+
+    @Ignore
+    void worldCountriesByPopulation() {
+    }
+
+    @Ignore
+    void continentCountriesByPopulation() {
+    }
+
+    @Ignore
+    void regionCountriesByPopulation() {
+    }
+
+    @Ignore
+    void worldHighestPopulationCountries() {
+    }
+
+    @Ignore
+    void continentHighestPopulationCountries() {
+    }
+
+    @Ignore
+    void regionHighestPopulationCountries() {
+    }
+
+    @Ignore
+    void worldCitiesByPopulation() {
+    }
+
+    @Ignore
+    void continentCitiesByPopulation() {
+    }
+
+    @Ignore
+    void regionCitiesByPopulation() {
+    }
+
+    @Ignore
+    void countryCitiesByPopulation() {
+    }
+
+    @Ignore
+    void districtCitiesByPopulation() {
+    }
+
+    @Ignore
+    void worldHighestPopulationCities() {
+    }
+
+    @Ignore
+    void continentHighestPopulationCities() {
+    }
+
+    @Ignore
+    void regionHighestPopulationCities() {
+    }
+
+    @Ignore
+    void countryHighestPopulationCities() {
+    }
+
+    @Ignore
+    void districtHighestPopulationCities() {
+    }
+
+    @Ignore
+    void worldCapitalsByPopulation() {
+    }
+
+    @Ignore
+    void ContinentCapitalsByPopulation() {
+    }
+
+    @Ignore
+    void RegionCapitalsByPopulation() {
+    }
+
+    @Ignore
+    void worldHighestPopulationCapitals() {
+    }
+
+    @Ignore
+    void continentHighestPopulationCapitals() {
+    }
+
+    @Ignore
+    void regionHighestPopulationCapitals() {
+    }
+
+    @Ignore
+    void continentCityPopulation() {
+    }
+
+    @Ignore
+    void continentRuralPopulation() {
+    }
+
+    @Ignore
+    void regionCityPopulation() {
+    }
+
+    @Ignore
+    void regionRuralPopulation() {
+    }
+
+    @Ignore
+    void countryCityPopulation() {
+    }
+
+    @Ignore
+    void countryRuralPopulation() {
+    }
+
+    @Ignore
+    void worldPopulation() {
+    }
+
+    @Ignore
+    void continentPopulation() {
+    }
+
+    @Ignore
+    void regionPopulation() {
+    }
+
+    @Ignore
+    void countryPopulation() {
+    }
+
+    @Ignore
+    void districtPopulation() {
+    }
+
+    @Ignore
+    void cityPopulation() {
+    }
+
+    @Ignore
+    void languageUsagePopulation() {
+    }
+
+    @Ignore
+    void worldLanguageUsagePercentage() {
+    }
+
+    @Ignore
+    void countryReport() {
+    }
+
+    @Ignore
+    void cityReport() {
+    }
+
+    @Ignore
+    void capitalCityReport() {
+    }
+
+    @Ignore
+    void populationReport() {
     }
 }
