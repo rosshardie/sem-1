fc6fe0ed0eb1aa646adb3ca81070852abebcee16
Dillon-Brown <dillon.brown@salesagility.com>
2019-02-05 16:21:27 +0000

    Remove @ignore

--- src/test/java/com/napier/sem/ReportTest.java
+++ src/test/java/com/napier/sem/ReportTest.java
@@ -17,163 +17,123 @@ public class ReportTest {
         report = new Report();
     }
 
-    @Ignore
     void worldCountriesByPopulation() {
     }
 
-    @Ignore
     void continentCountriesByPopulation() {
     }
 
-    @Ignore
     void regionCountriesByPopulation() {
     }
 
-    @Ignore
     void worldHighestPopulationCountries() {
     }
 
-    @Ignore
     void continentHighestPopulationCountries() {
     }
 
-    @Ignore
     void regionHighestPopulationCountries() {
     }
 
-    @Ignore
     void worldCitiesByPopulation() {
     }
 
-    @Ignore
     void continentCitiesByPopulation() {
     }
 
-    @Ignore
     void regionCitiesByPopulation() {
     }
 
-    @Ignore
     void countryCitiesByPopulation() {
     }
 
-    @Ignore
     void districtCitiesByPopulation() {
     }
 
-    @Ignore
     void worldHighestPopulationCities() {
     }
 
-    @Ignore
     void continentHighestPopulationCities() {
     }
 
-    @Ignore
     void regionHighestPopulationCities() {
     }
 
-    @Ignore
     void countryHighestPopulationCities() {
     }
 
-    @Ignore
     void districtHighestPopulationCities() {
     }
 
-    @Ignore
     void worldCapitalsByPopulation() {
     }
 
-    @Ignore
     void ContinentCapitalsByPopulation() {
     }
 
-    @Ignore
     void RegionCapitalsByPopulation() {
     }
 
-    @Ignore
     void worldHighestPopulationCapitals() {
     }
 
-    @Ignore
     void continentHighestPopulationCapitals() {
     }
 
-    @Ignore
     void regionHighestPopulationCapitals() {
     }
 
-    @Ignore
     void continentCityPopulation() {
     }
 
-    @Ignore
     void continentRuralPopulation() {
     }
 
-    @Ignore
     void regionCityPopulation() {
     }
 
-    @Ignore
     void regionRuralPopulation() {
     }
 
-    @Ignore
     void countryCityPopulation() {
     }
 
-    @Ignore
     void countryRuralPopulation() {
     }
 
-    @Ignore
     void worldPopulation() {
     }
 
-    @Ignore
     void continentPopulation() {
     }
 
-    @Ignore
     void regionPopulation() {
     }
 
-    @Ignore
     void countryPopulation() {
     }
 
-    @Ignore
     void districtPopulation() {
     }
 
-    @Ignore
     void cityPopulation() {
     }
 
-    @Ignore
     void languageUsagePopulation() {
     }
 
-    @Ignore
     void worldLanguageUsagePercentage() {
     }
 
-    @Ignore
     void countryReport() {
     }
 
-    @Ignore
     void cityReport() {
     }
 
-    @Ignore
     void capitalCityReport() {
     }
 
-    @Ignore
     void populationReport() {
     }
 
