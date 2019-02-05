678b3e2582dc5343d2b028e252e6f5e8390232ae
Dillon-Brown <dillon.brown@salesagility.com>
2019-02-05 16:51:19 +0000

    @Test

--- src/test/java/com/napier/sem/TestReport.java
+++ src/test/java/com/napier/sem/TestReport.java
@@ -17,87 +17,115 @@ public class TestReport {
         report = new Report();
     }
 
+    @Test
     void worldCountriesByPopulation() {
     }
 
+    @Test
     void continentCountriesByPopulation() {
     }
 
+    @Test
     void regionCountriesByPopulation() {
     }
 
+    @Test
     void worldHighestPopulationCountries() {
     }
 
+    @Test
     void continentHighestPopulationCountries() {
     }
 
+    @Test
     void regionHighestPopulationCountries() {
     }
 
+    @Test
     void worldCitiesByPopulation() {
     }
 
+    @Test
     void continentCitiesByPopulation() {
     }
 
+    @Test
     void regionCitiesByPopulation() {
     }
 
+    @Test
     void countryCitiesByPopulation() {
     }
 
+    @Test
     void districtCitiesByPopulation() {
     }
 
+    @Test
     void worldHighestPopulationCities() {
     }
 
+    @Test
     void continentHighestPopulationCities() {
     }
 
+    @Test
     void regionHighestPopulationCities() {
     }
 
+    @Test
     void countryHighestPopulationCities() {
     }
 
+    @Test
     void districtHighestPopulationCities() {
     }
 
+    @Test
     void worldCapitalsByPopulation() {
     }
 
+    @Test
     void ContinentCapitalsByPopulation() {
     }
 
+    @Test
     void RegionCapitalsByPopulation() {
     }
 
+    @Test
     void worldHighestPopulationCapitals() {
     }
 
+    @Test
     void continentHighestPopulationCapitals() {
     }
 
+    @Test
     void regionHighestPopulationCapitals() {
     }
 
+    @Test
     void continentCityPopulation() {
     }
 
+    @Test
     void continentRuralPopulation() {
     }
 
+    @Test
     void regionCityPopulation() {
     }
 
+    @Test
     void regionRuralPopulation() {
     }
 
+    @Test
     void countryCityPopulation() {
     }
 
+    @Test
     void countryRuralPopulation() {
     }
 
@@ -105,39 +133,51 @@ public class TestReport {
     void worldPopulation() {
     }
 
+    @Test
     void continentPopulation() {
     }
 
+    @Test
     void regionPopulation() {
     }
 
+    @Test
     void countryPopulation() {
     }
 
+    @Test
     void districtPopulation() {
     }
 
+    @Test
     void cityPopulation() {
     }
 
+    @Test
     void languageUsagePopulation() {
     }
 
+    @Test
     void worldLanguageUsagePercentage() {
     }
 
+    @Test
     void countryReport() {
     }
 
+    @Test
     void cityReport() {
     }
 
+    @Test
     void capitalCityReport() {
     }
 
+    @Test
     void populationReport() {
     }
 
+    @Test
     void getCountryList() {
 
     }
