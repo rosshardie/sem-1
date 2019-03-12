4acfadfd3cd2dc31145903c3aa76f3e5860b52dd
Dillon-Brown <dillon.brown@salesagility.com>
2019-03-12 07:23:30 +0000

    City + Country classes

---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+/**
+ * Represents a city
+ */
+public class City {
+    /**
+     * City ID
+     */
+    public int ID;
+
+    /**
+     * City Name
+     */
+    public String Name;
+
+    /**
+     * City CountryCode
+     */
+    public String CountryCode;
+
+    /**
+     * City District
+     */
+    public String District;
+
+    /**
+     * City Population
+     */
+    public int Population;
+}
