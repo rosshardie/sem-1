689a887fff0e6c6b1745a6444f7b81d63133b0f0
Dillon-Brown <dillon.brown@salesagility.com>
2019-03-12 11:59:48 +0000

    Report menu

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 import java.util.ArrayList;
 import java.sql.*;
+import java.util.Scanner;
 
 public class App {
 
@@ -21,29 +22,72 @@ public class App {
             db.connect(args[0]);
         }
         // Main report
-        a.mainReport();
+        a.menu();
 
         // Disconnect DB
         db.disconnect();
     }
 
     /**
-     * Runs the main report
+     * Select a report menu
      */
-    private void mainReport() {
-        System.out.println("All countries by population from largest to smallest:\n");
-        ArrayList<Country> worldCountries = this.worldCountriesByPopulationLS();
-        this.printCountries(worldCountries);
+    private void menu() {
+        System.out.println("Select a function:");
+        System.out.println("1 All the countries in the world organised by largest population to smallest.\n");
+        System.out.println("2 All the countries in a continent organised by largest population to smallest.\n");
+        System.out.println("3 All the countries in a region organised by largest population to smallest.\n");
+        System.out.println("4 The top N populated countries in the world where N is provided by the user.\n");
+        System.out.println("5 The top N populated countries in a continent where N is provided by the user.\n");
+        System.out.println("6 The top N populated countries in a region where N is provided by the user.\n");
+        System.out.println("7 All the cities in the world organised by largest population to smallest.\n");
+        System.out.println("8 All the cities in a continent organised by largest population to smallest.\n");
+        System.out.println("9 All the cities in a region organised by largest population to smallest.\n");
+        System.out.println("10 All the cities in a country organised by largest population to smallest.\n");
+        System.out.println("11 All the cities in a district organised by largest population to smallest.\n");
+        System.out.println("12 The top N populated cities in the world where N is provided by the user.\n");
+        System.out.println("13 The top N populated cities in a continent where N is provided by the user.\n");
+        System.out.println("14 The top N populated cities in a region where N is provided by the user.\n");
+        System.out.println("15 The top N populated cities in a country where N is provided by the user.\n");
+        System.out.println("16 The top N populated cities in a district where N is provided by the user.\n");
+        System.out.println("17 All the capital cities in the world organised by largest population to smallest.\n");
+        System.out.println("18 All the capital cities in a continent organised by largest population to smallest.\n");
+        System.out.println("19 All the capital cities in a region organised by largest to smallest.\n");
+        System.out.println("20 The top N populated capital cities in the world where N is provided by the user.\n");
+        System.out.println("21 The top N populated capital cities in a continent where N is provided by the user.\n");
+        System.out.println("22 The top N populated capital cities in a region where N is provided by the user.\n");
+        System.out.println("23 The population of people, people living in cities, and people not living in cities in each continent.\n");
+        System.out.println("24 The population of people, people living in cities, and people not living in cities in each region.\n");
+        System.out.println("25 The population of people, people living in cities, and people not living in cities in each country.\n");
+        System.out.println("26 The population of the world.\n");
+        System.out.println("27 The population of a continent.\n");
+        System.out.println("28 The population of a region.\n");
+        System.out.println("29 The population of a country.\n");
+        System.out.println("30 The population of a district.\n");
+        System.out.println("31 The population of a city.\n");
+        System.out.println("32 Chinese.\n");
+        System.out.println("33 English.\n");
+        System.out.println("34 Hindi.\n");
+        System.out.println("35 Spanish.\n");
+        System.out.println("361 Arabic.\n");
 
-        System.out.println("All the countries in a continent organised by largest population to smallest:\n");
-        String continent = "Asia";
-        ArrayList<Country> continentCountries = this.continentCountriesByPopulationLS(continent);
-        this.printCountries(continentCountries);
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
 
-        System.out.println("All the countries in a region organised by largest population to smallest.\n:\n");
-        String region = "Middle East";
-        ArrayList<Country> regionCountries = this.regionCountriesByPopulationLS(region);
-        this.printCountries(continentCountries);
+        if (i == 1) {
+            System.out.println("All countries by population from largest to smallest:\n");
+            ArrayList<Country> worldCountries = this.worldCountriesByPopulationLS();
+            this.printCountries(worldCountries);
+        } else if (i == 2) {
+            System.out.println("All the countries in a continent organised by largest population to smallest:\n");
+            String continent = "Asia";
+            ArrayList<Country> continentCountries = this.continentCountriesByPopulationLS(continent);
+            this.printCountries(continentCountries);
+        } else if (i == 3) {
+            System.out.println("All the countries in a region organised by largest population to smallest.\n:\n");
+            String region = "Middle East";
+            ArrayList<Country> regionCountries = this.regionCountriesByPopulationLS(region);
+            this.printCountries(regionCountries);
+        }
     }
 
     /**
